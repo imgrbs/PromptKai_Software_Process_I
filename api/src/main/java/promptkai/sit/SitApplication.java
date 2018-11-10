6be8ba312a5ca66f6ddcdff48085529f589c623e
Keerati <keerati.jear@gmail.com>
2018-11-10 12:36:56 +0700

    [ADD] init api

---
+++ api/src/main/java/promptkai/sit/SitApplication.java
@@ -0,0 +1,12 @@
+package promptkai.sit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitApplication.class, args);
+	}
+}
