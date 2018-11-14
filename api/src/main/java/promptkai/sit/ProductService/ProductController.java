d77eceb94d0735a493113a991e9956cc47cb1e51
kan17415 <kannatad@hotmail.com>
2018-11-14 12:16:36 +0700

    [UPDATE] แก้ @GetMapping(name="") เป็น @GetMapping(value="")

--- api/src/main/java/promptkai/sit/ProductService/ProductController.java
+++ api/src/main/java/promptkai/sit/ProductService/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
@@ -12,16 +13,17 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping(name="/products")
+    @GetMapping(value="/products")
     public List<Product> getProducts(){
         return productRepository.findAll();
     }
 
-    @GetMapping(name="/product/{productid}")
+    @GetMapping(value="/product/{productid}")
     public ResponseEntity<Product> getProducts(@PathVariable String productid, HttpServletResponse res){
         Optional<Product> productOptional = productRepository.findById(Long.parseLong(productid));
         return productOptional.isPresent() ? new ResponseEntity<Product>(productOptional.get(), HttpStatus.OK) : new ResponseEntity<Product>(HttpStatus.NOT_FOUND);
