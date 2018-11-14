f46a6a2061e6017dc2f87584be4cd7a48cf11097
moshirosang <tiw_tisadee@hotmail.com>
2018-11-14 12:21:43 +0700

    [ADD] add orderlist model

---
+++ api/src/main/java/promptkai/sit/OrderService/OrderList.java
@@ -0,0 +1,15 @@
+package promptkai.sit.OrderService;
+
+import java.util.List;
+
+public class OrderList {
+    private List<OrderList> orderLists;
+
+    public List<OrderList> getOrderLists() {
+        return orderLists;
+    }
+
+    public void setOrderLists(List<OrderList> orderLists) {
+        this.orderLists = orderLists;
+    }
+}
