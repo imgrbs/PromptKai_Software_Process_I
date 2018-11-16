d0a17ce334fd5474f3fc008a1fbcf139bc436804
Keerati <keerati.jear@gmail.com>
2018-11-16 15:21:04 +0700

    [ADD] Add crossorigin

--- api/src/main/java/promptkai/sit/PaymentService/PaymentController.java
+++ api/src/main/java/promptkai/sit/PaymentService/PaymentController.java
@@ -3,10 +3,7 @@ package promptkai.sit.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import promptkai.sit.ProductService.Product;
+import org.springframework.web.bind.annotation.*;
 
 
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin("*")
 public class PaymentController {
     @Autowired
     private PaymentRepository paymentRepository;
--- api/src/main/java/promptkai/sit/ProductService/ProductController.java
+++ api/src/main/java/promptkai/sit/ProductService/ProductController.java
@@ -3,17 +3,14 @@ package promptkai.sit.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-
+@CrossOrigin("*")
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
