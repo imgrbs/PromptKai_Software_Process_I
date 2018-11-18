3ca130fd438404f05f1e113f00815e9873d4f432
jirapa songchom <jirapa.songchom@mail.kmutt.ac.th>
2018-11-18 22:25:59 +0700

    [UPDATE] Add tag a href for showing product detail

--- frontend/assets/js/index.js
+++ frontend/assets/js/index.js
@@ -4,14 +4,15 @@ $(document).ready(function () {
             var productElements = '';
             data.forEach(function (product) {
                 productElements +=
-                    '<div class="col-6 col-lg-3 text-center mb-3">' +
+                    '<a style="color:black ; text-decoration: none" href="product_detail.html?productid="'+
+                    product.id+'><div class="col-6 col-lg-3 text-center mb-3">' +
                     '<div class="card border-0">' +
                     '<img class="card-img-top" src="assets/img/glass.png" alt="image">' +
                     '<div class="card-body p-0">' +
                     '<h5 class="mt-3 mb-0"><b>' + product.productName + '</b></h5>' +
                     'รหัสสินค้า' + product.id + '<br>' +
                     product.productPrice + 'บาท<br>' +
-                    '</div></div></div>'    ;
+                    '</div></div></div></a>'    ;
             })
             $('#products').html(productElements);
         } else {
