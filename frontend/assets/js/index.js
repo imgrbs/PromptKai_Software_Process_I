b50d8f15f8e96b47d7636352c00e4a0102175f84
jirapa songchom <jirapa.songchom@mail.kmutt.ac.th>
2018-11-18 21:58:00 +0700

    [UPDATE] Move code show product into index.js

--- frontend/assets/js/index.js
+++ frontend/assets/js/index.js
@@ -1,8 +1,19 @@
 $(document).ready(function () {
     $.get("http://promptkai-api-bright-chimpanzee.mybluemix.net/products", function (data, status) {
         if (status == 'success') {
-            var products = data[0]
-                document.getElementById("products")
+            var productElements = '';
+            data.forEach(function (product) {
+                productElements +=
+                    '<div class="col-6 col-lg-3 text-center mb-3">' +
+                    '<div class="card border-0">' +
+                    '<img class="card-img-top" src="assets/img/glass.png" alt="image">' +
+                    '<div class="card-body p-0">' +
+                    '<h5 class="mt-3 mb-0"><b>' + product.productName + '</b></h5>' +
+                    'รหัสสินค้า' + product.id + '<br>' +
+                    product.productPrice + 'บาท<br>' +
+                    '</div></div></div>'    ;
+            })
+            $('#products').html(productElements);
         } else {
             $('div#text h1').text('Fetch Errors :(')
         }
