7266bb5b751fde952abdde65ac300d78249c3e1d
jirapa songchom <jirapa.songchom@mail.kmutt.ac.th>
2018-11-18 23:21:46 +0700

    [UPDATE] Edit tag a

--- frontend/assets/js/index.js
+++ frontend/assets/js/index.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
             var productElements = '';
             data.forEach(function (product) {
                 productElements +=
-                    '<a style="color:black ; text-decoration: none" href="product_detail.html?productid="'+
-                    product.id+'><div class="col-6 col-lg-3 text-center mb-3">' +
+                    '<a style="color:black ; text-decoration: none" href="product_detail.html?productid='+
+                    product.id+'"><div class="col-6 col-lg-3 text-center mb-3">' +
                     '<div class="card border-0">' +
                     '<img class="card-img-top" src="assets/img/glass.png" alt="image">' +
                     '<div class="card-body p-0">' +
