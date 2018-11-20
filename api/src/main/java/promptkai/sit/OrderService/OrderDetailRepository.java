e9a09bfa9c2708bd6b9b6facf7505f9c6dcd202b
Keerati <keerati.jear@gmail.com>
2018-11-20 23:19:52 +0700

    [UPDATE] update save order detail when order proceed

---
+++ api/src/main/java/promptkai/sit/OrderService/OrderDetailRepository.java
@@ -0,0 +1,8 @@
+package promptkai.sit.OrderService;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+}
