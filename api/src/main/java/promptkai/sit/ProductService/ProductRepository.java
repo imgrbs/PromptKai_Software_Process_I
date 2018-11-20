d65bc68f54300a44f11874e99396834e2cad63fc
poom <krittaphun.p@mail.kmutt.ac.th>
2018-11-20 22:45:53 +0700

    [UPDATE] productRepo to old version

--- api/src/main/java/promptkai/sit/ProductService/ProductRepository.java
+++ api/src/main/java/promptkai/sit/ProductService/ProductRepository.java
@@ -2,12 +2,9 @@ package promptkai.sit.ProductService;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
-    @Query("SELECT p FROM products p WHERE p.product_type = :productType")
-    public List<Product> findByProductType(@Param("productType") String productType);
+    List<Product> findByProductType(String productType);
 }
