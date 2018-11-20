79d01108a619ebdf9ba5b2a84f1a6f7edd33d776
Keerati <keerati.jear@gmail.com>
2018-11-20 10:58:18 +0700

    [UPDATE] update logo in omise

--- frontend/assets/js/payment/payment_credit.js
+++
@@ -1,7 +0,0 @@
-var image = window.location.host + '/assets/img/logo.png';
-
-$(document).ready(function () {
-    $('button.omise-checkout-button').click(function () {
-        document.querySelector('.OmiseCheckoutForm img').src = image;
-    });
-});
