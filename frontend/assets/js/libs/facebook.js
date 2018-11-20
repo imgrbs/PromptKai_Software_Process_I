e4f28c79f0fe5ee764eac130a59ef02a28e5cfd6
hippiejam <Nitjawan.jam@mail.kmutt.ac.th>
2018-11-20 21:03:00 +0700

    [UPDATE] Update facebook sdk
    [UPDATE] Update facebook sdk

--- frontend/assets/js/libs/facebook.js
+++ frontend/assets/js/libs/facebook.js
@@ -1,3 +1,12 @@
+// Load the SDK asynchronously
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
 function statusChangeCallback(response) {
     console.log('statusChangeCallback');
     if (response.status === 'connected') {
@@ -15,6 +24,9 @@ function checkLoginState() {
 window.fbAsyncInit = function() {
     FB.init({
         appId      : '257585084911795',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v2.8'
     });
 
     FB.getLoginStatus(function(response) {
