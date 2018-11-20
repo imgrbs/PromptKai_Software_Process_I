a00977ee2ddf8c6e88fd039e22573bd61154d41d
Keerati <keerati.jear@gmail.com>
2018-11-20 19:31:22 +0700

    [UPDATE] update payment total prices

--- frontend/assets/js/payment/payment.js
+++ frontend/assets/js/payment/payment.js
@@ -1,1 +1,9 @@
-document.getElementsByClassName('omise-checkout-button')[0].className = 'omise-checkout-button fixed-bottom w-100 text-center button-primary pt-2 pb-1'
+document.getElementsByClassName('omise-checkout-button')[0].className = 'omise-checkout-button fixed-bottom w-100 text-center button-primary pt-2 pb-1';
+
+const params = new URLSearchParams(window.location.search);
+const productprice = params.get('productprice');
+const quantity = params.get('quantity');
+const totalPrice = quantity * productprice
+
+document.getElementById('description').value = 'Product order ฿' + totalPrice + '.00'
+document.getElementById('omise').setAttribute('data-amount', totalPrice * 100)
