4d8f33b72593ff84121cb7bf97674b5ac2b801dd
poom <krittaphun.p@mail.kmutt.ac.th>
2018-11-20 22:28:10 +0700

    [UPDATE] api import query, param

--- api/src/main/java/promptkai/sit/ProductService/ProductRepository.java
+++ api/src/main/java/promptkai/sit/ProductService/ProductRepository.java
@@ -2,6 +2,8 @@ package promptkai.sit.ProductService;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 @Repository
