b092d6256a02cd736901154c2c71285d491d77f9
Keerati <keerati.jear@gmail.com>
2018-11-20 10:56:00 +0700

    [UPDATE] update logo in omise element

--- frontend/assets/js/payment/payment.js
+++ frontend/assets/js/payment/payment.js
@@ -1,1 +1,1 @@
-document.getElementsByClassName('omise-checkout-button')[0].className = 'fixed-bottom w-100 text-center button-primary pt-2 pb-1'
+document.getElementsByClassName('omise-checkout-button')[0].className = 'omise-checkout-button fixed-bottom w-100 text-center button-primary pt-2 pb-1'
--- frontend/assets/js/payment/payment_credit.js
+++ frontend/assets/js/payment/payment_credit.js
@@ -1,1 +1,7 @@
-document.querySelector('.OmiseCheckoutForm img').src = window.location.host + '/assets/img/logo.png';
+var image = window.location.host + '/assets/img/logo.png';
+
+$(document).ready(function () {
+    $('button.omise-checkout-button').click(function () {
+        document.querySelector('.OmiseCheckoutForm img').src = image;
+    });
+});
